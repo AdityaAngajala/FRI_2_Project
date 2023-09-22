9bbc67f428c4e2ad0983968408b1ba3379b52478
Krish Agarwal <krishagarwal@live.com>
2023-09-22 14:30:08 -0500

    very basic topology generator

---
+++ topology_generator.py
@@ -0,0 +1,36 @@
+import noise
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+
+shape = (500,500)
+scale = 200.0
+octaves = 6
+persistence = 0.5
+lacunarity = 2.0
+
+world = np.zeros(shape)
+for i in range(shape[0]):
+    for j in range(shape[1]):
+        world[i][j] = noise.pnoise2(i/scale, 
+                                    j/scale, 
+                                    octaves=octaves, 
+                                    persistence=persistence, 
+                                    lacunarity=lacunarity, 
+                                    repeatx=1024, 
+                                    repeaty=1024, 
+                                    base=42)
+
+plt.figure(figsize=(8, 8))
+plt.imshow(world,cmap='terrain')
+plt.savefig('topological_map.png')
+plt.show()
+
+lin_x = np.linspace(0,1,shape[0],endpoint=False)
+lin_y = np.linspace(0,1,shape[1],endpoint=False)
+x,y = np.meshgrid(lin_x,lin_y)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+ax.plot_surface(x,y,world,cmap='terrain')
+plt.savefig('3d_vizualization.png')
+plt.show()
