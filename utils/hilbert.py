c56e68fe69d5b02f792ad7e575b67df396d43038
AdityaAngajala <53411299+AdityaAngajala@users.noreply.github.com>
2023-10-19 12:38:30 -0500

    Hilbert Space Filling Curve Experiment

---
+++ utils/hilbert.py
@@ -0,0 +1,227 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import pyvista as pv
+from mpl_toolkits.mplot3d import Axes3D
+
+N = 3
+M = 6
+
+
+def bin_str(i):
+    """Return a string representation of i with N bits."""
+    out = ''
+    for j in range(N - 1, -1, -1):
+        if (i >> j) & 1 == 1:
+            out += '1'
+        else:
+            out += '0'
+    return out
+
+
+def rotate_right(x, d):
+    """Rotate x by d bits to the right."""
+    d = d % N
+    out = x >> d
+    for i in range(d):
+        bit = (x & (2 ** i)) >> i
+        out |= bit << (N + i - d)
+    return out
+
+
+def rotate_left(x, d):
+    """Rotate x by d bits to the left."""
+    d = d % N
+    out = x << d
+    excess = out
+    out = out & (2 ** N - 1)
+    for i in range(d):
+        bit = (x & (2 ** (N - 1 - d + 1 + i))) >> (N - 1 - d + 1 + i)
+        out |= bit << i
+    return out
+
+
+def bit_component(x, i):
+    """Return the i-th bit of x"""
+    return (x & (2 ** i)) >> i
+
+
+# verify that '~i & 2**N-1' performs the NOT operation in N-bit space
+for i in range(2 ** N):
+    not_i = ~i & (2 ** N - 1)
+    assert not_i >= 0
+    assert not_i < 2 ** N
+    assert i & not_i == 0
+    assert i | not_i == (2 ** N - 1)
+
+
+# Define other functions and variables
+
+def gc(i):
+    """Return the Gray code index of i."""
+    return i ^ (i >> 1)
+
+
+def e(i):
+    """Return the entry point of hypercube i."""
+    if i == 0:
+        return 0
+    else:
+        return gc(2 * int(math.floor((i - 1) // 2)))
+
+
+def f(i):
+    """Return the exit point of hypercube i."""
+    return e(2 ** N - 1 - i) ^ 2 ** (N - 1)
+
+
+def i_to_p(i):
+    """Extract the 3D position from a 3-bit integer."""
+    return [bit_component(i, j) for j in (0, 1, 2)]
+
+
+def add_edges(edges):
+    """Extend the list of edges from a hypercube to the list of
+    edges of the hypercube of the next dimension."""
+    old_edges = list(edges)
+    old_points = set([x[0] for x in old_edges]) | set([x[1] for x in old_edges])
+    edges = [((0,) + x[0], (0,) + x[1]) for x in old_edges]
+    edges.extend([((1,) + x[0], (1,) + x[1]) for x in old_edges])
+    for e in old_points:
+        edges.append(((0,) + e, (1,) + e))
+    return edges
+
+
+def set_unit_cube(ax, side=1, set_view=(10, -67)):
+    """Present the unit cube."""
+    # ax.set_xlabel('x')
+    # ax.set_ylabel('y')
+    # ax.set_zlabel('z')
+    # ax.set_xticks(range(side + 1))
+    # ax.set_yticks(range(side + 1))
+    # ax.set_zticks(range(side + 1))
+    # ax.set_xlim(0, side)
+    # ax.set_ylim(0, side)
+    # ax.set_zlim(0, side)
+    if set_view:
+        ax.view_init(*set_view)
+
+
+def inverse_gc(g):
+    """The inverse gray code."""
+    i = g
+    j = 1
+    while j < N:
+        i = i ^ (g >> j)
+        j = j + 1
+    return i
+
+
+def g(i):
+    """The direction between subcube i and the next one"""
+    return int(np.log2(gc(i) ^ gc(i + 1)))
+
+
+def d(i):
+    """The direction of the arrow whithin a subcube."""
+    if i == 0:
+        return 0
+    elif (i % 2) == 0:
+        return g(i - 1) % N
+    else:
+        return g(i) % N
+
+
+def T(e, d, b):
+    """Transform b."""
+    out = b ^ e
+    return rotate_right(out, d + 1)
+
+
+def T_inv(e, d, b):
+    """Inverse transform b."""
+    return T(rotate_right(e, d + 1), N - d - 2, b)
+
+
+def TR_algo2(p):
+    """Return the Hilbert index of point p"""
+    # h will contain the Hilbert index
+    h = 0
+    # ve and vd contain the entry point and dimension of the current subcube
+    # we choose here a main traversal direction N-2 (i.e. z for a cube) to match
+    # the illustrations
+    ve = 0
+    vd = 2
+    for i in range(M - 1, -1, -1):
+        # the cell label is constructed in two steps
+        # 1. extract the relevant bits from p
+        l = [bit_component(px, i) for px in p]
+        # 2. construct a integer whose bits are given by l
+        l = sum([lx * 2 ** j for j, lx in enumerate(l)])
+        # transform l into the current subcube
+        l = T(ve, vd, l)
+        # obtain the gray code ordering from the label l
+        w = inverse_gc(l)
+        # compose (see [TR] lemma 2.13) the transform of ve and vd
+        # with the data of the subcube
+        ve = ve ^ (rotate_left(e(w), vd + 1))
+        vd = (vd + d(w) + 1) % N
+        # move the index to more significant bits and add current value
+        h = (h << N) | w
+    return h
+
+
+def TR_algo3(h):
+    """Return the coordinates for the Hilbert index h"""
+    ve = 0
+    vd = 2
+    p = [0] * N
+    for i in range(M - 1, -1, -1):
+        w = [bit_component(h, i * N + ii) for ii in range(N)]
+        # print(i, w)
+        w = sum([wx * 2 ** j for j, wx in enumerate(w)])
+        # print(i, w, gc(w))
+        l = gc(w)
+        l = T_inv(ve, vd, l)
+        for j in range(N):
+            p[j] += bit_component(l, j) << i
+        ve = ve ^ rotate_left(e(w), vd + 1)
+        vd = (vd + d(w) + 1) % N
+    return p
+
+
+def gen_coords(dimSize, size_exponent):
+    pointsX = []
+    pointsY = []
+    pointsZ = []
+    # points = []
+    for h in range(2 ** (dimSize * size_exponent)):
+        x, y, z = TR_algo3(h)
+        pointsX.append(x)
+        pointsY.append(y)
+        pointsZ.append(z)
+        # points.append(x, y, z)
+        # print(x, y, z)
+    return pointsX, pointsY, pointsZ
+
+if __name__ == '__main__':
+    x, y, z = gen_coords(N, M)
+
+    print(len(x))
+
+    points = np.column_stack((x, y, z))
+    cloud = pv.PolyData(points)
+
+    plotter = pv.Plotter()
+
+    # Add the 3D scatter plot
+    # plotter.add_points(cloud, color='b', point_size=5, render_points_as_spheres=True)
+
+    # Add lines to connect points
+    for i in range(1, len(x)):
+        line = pv.Line([x[i - 1], y[i - 1], z[i - 1]], [x[i], y[i], z[i]])
+        plotter.add_mesh(line, color='r')
+
+    # Display the plot
+    plotter.show()
--- voxelGeneration.py
+++ voxelGeneration.py
@@ -1,8 +1,13 @@
+import math
+import random
+
+import cv2
 import mplcursors
 import numpy as np
 import vtk
 from matplotlib import pyplot as plt
 from matplotlib import colors
+from bad.hilbert import gen_coords
 import pyvista as pv
 from pyvista.plotting.opts import PickerType
 from topologyLabelGeneration import generate_noise
@@ -11,19 +16,14 @@ from topologyLabelGeneration import generate_noise
 class Const:
     LAND_SIZE = 64
     NUM_COLORS = 24
+    IMAGE_HEIGHT_CAP = 512
 
 
 data = []
+colorOutput = []
 
 
-def generate_terrain():
-    height_map = np.zeros((Const.LAND_SIZE, Const.LAND_SIZE)).astype('float64')
-    height_map += generate_noise(size=Const.LAND_SIZE)
-    height_map = height_map / np.max(np.abs(height_map)) * Const.LAND_SIZE * (40 / 128)
-    return height_map
-
-
-def generate_2d_plot(plot, name, save=True):
+def plot_slice(plot, name, save=True):
     fig, ax = plt.subplots(figsize=(8, 8))
     ax.axis('off')
 
@@ -53,6 +53,166 @@ def generate_2d_plot(plot, name, save=True):
         sel.annotation.set_text(text)
 
 
+def plot_sparse(input_data):
+    outputPlot = np.full((Const.LAND_SIZE ** 2 * 2, Const.LAND_SIZE), np.nan)
+
+    for index in range(len(input_data)):
+        colorVals = [val[0] for val in input_data[index]]
+        lenVals = [val[1] for val in input_data[index]]
+
+        for index2 in range(len(colorVals)):
+            outputPlot[index * 2][index2] = colorVals[index2]
+            outputPlot[index * 2 + 1][index2] = lenVals[index2]
+
+    max_len = np.max([len(col) for col in input_data])
+    print("MAX LEN: ", max_len)
+    outputPlot = np.where(outputPlot < 0, 0, outputPlot)
+    outputPlot = np.where(outputPlot > 23.5, 23.5, outputPlot)
+    init_colors()
+
+    write_image_interleave(outputPlot, max_len, upscale=1)
+
+
+def sparse_data(input_data):
+    columnSum = 0
+    sparseListSum = 0
+    prefilterSum = 0
+
+    data_sparse = []
+    data_prefilter = []
+
+    for x in range(Const.LAND_SIZE):
+        for y in range(Const.LAND_SIZE):
+            column = input_data[y, x, :]  # Get Every Z Column
+
+            data_sparse.append(sparse_list(column))
+            data_prefilter.append(sparse_list(prefilter_sub(column)))
+
+            # printColInfo(column)
+
+            columnSum += np.count_nonzero(~np.isnan(column))
+            sparseListSum += len(sparse_list(column)) * 2 - 2
+            prefilterSum += len(sparse_list(prefilter_sub(column))) * 2 - 2
+
+    # printAvgColInfo(columnSum, sparseListSum, prefilterSum, data_sparse, data_prefilter)
+    plot_sparse(data_sparse)
+
+
+def sparse_list(input_list):
+    list_sparse = []
+    count = 0
+    prevVal = input_list[0]
+    input_list = np.where(np.isnan(input_list), np.min, input_list)
+    for val in input_list:
+        if not (val == prevVal):
+            if prevVal == np.min:
+                prevVal = np.nan
+            list_sparse.append((prevVal, count))
+            count = 0
+        prevVal = val
+        count += 1
+    if prevVal == np.min:
+        prevVal = np.nan
+    list_sparse.append((prevVal, count))
+    return list_sparse
+
+
+def printColInfo(column):
+    print("Column: ", column)
+    print("Column len: ", np.count_nonzero(~np.isnan(column)))
+    print("Sparselist: ", sparse_list(column))
+    print("Sparselist len: ", len(sparse_list(column)) * 2 - 2)
+    print("Prefilter: ", sparse_list(prefilter_sub(column)))
+    print("Prefilter Len: ", len(sparse_list(prefilter_sub(column))) * 2 - 2)
+
+
+def printAvgColInfo(columnSum, sparseListSum, prefilterSum, data_sparse, data_prefilter):
+    print("Column: ", columnSum)
+    print("Avg: ", columnSum / Const.LAND_SIZE ** 2)
+    print("Sparselist: ", sparseListSum)
+    print("Avg: ", sparseListSum / Const.LAND_SIZE ** 2)
+    print("Max: ", np.max([len(col) for col in data_sparse]))
+    print("Prefilter: ", prefilterSum)
+    print("Avg: ", prefilterSum / Const.LAND_SIZE ** 2)
+    print("Max: ", np.max([len(col) for col in data_prefilter]))
+
+
+def prefilter_sub(input_list):
+    prevVal = 0
+    output = []
+    for val in input_list:
+        output.append(val - prevVal)
+        prevVal = val
+    return output
+
+
+def write_image_interleave(outputPlot, max_len, upscale=1):
+    height_cap = Const.IMAGE_HEIGHT_CAP
+    image = np.full((height_cap, (len(outputPlot) // height_cap) * max_len, 3), np.nan)
+
+    count = 0
+    for i in range(len(outputPlot)):
+        for j in range(max_len):
+            outputI = i % height_cap
+            outputJ = (count // height_cap) * max_len + j
+            # print("I,J: ", outputI, ", ", outputJ)
+            if np.isnan(outputPlot[i][j]):
+                image[outputI][outputJ] = (255, 255, 255)
+            else:
+                if i % 2 == 0:
+                    image[outputI][outputJ] = colorOutput[np.floor(outputPlot[i][j]).astype(int)]
+                    # Flipping color channel from RGB to BGR
+                    image[outputI][outputJ] = np.flip(image[outputI][outputJ])
+                else:
+                    val = outputPlot[i][j] * 10
+                    if val > 255:
+                        val = outputPlot[i][j]
+                    image[outputI][outputJ] = (val, val, val)
+        count += 1
+
+    cv2.imwrite('images3D/' + 'sdkjfsdkfj.png', upscale_image(image, upscale=upscale))
+
+
+def upscale_image(image, upscale=1):
+    shape = (image.shape[0] * upscale, image.shape[1] * upscale, image.shape[2])
+    image_upscale = np.full(shape, np.nan)
+    for a in range(upscale):
+        for b in range(upscale):
+            image_upscale[a::upscale, b::upscale] = image
+    return image_upscale
+
+
+# Really only works for 64x64x64 because the next size that get you a square image is 256x256x256 which is massive
+def plot_hilbert(input_data):
+    hilbert_x, hilbert_y, hilbert_z = gen_coords(dimSize=3, size_exponent=round(math.log2(Const.LAND_SIZE)))
+    image = np.full((round(math.sqrt(Const.LAND_SIZE ** 3)), round(math.sqrt(Const.LAND_SIZE ** 3)), 3), np.nan)
+
+    input_data = np.where(input_data < 0, 0, input_data)
+    input_data = np.where(input_data > 23.5, 23.5, input_data)
+
+    num = 0
+    print(len(image))
+    print("HILBER", len(hilbert_x))
+    for i in range(len(image)):
+        for j in range(len(image)):
+
+            val = input_data[hilbert_z[num]][hilbert_y[num]][hilbert_x[num]]
+            if np.isnan(val):
+                image[i][j] = (255, 255, 255)
+            else:
+                try:
+                    image[i][j] = colorOutput[np.floor(val).astype(int)]
+                except IndexError:
+                    print(i, j)
+                    print(val)
+                    print(np.floor(val).astype(int))
+                    print(colorOutput[val])
+            num += 1
+
+    image = image.reshape(2048, 128, 3)
+    cv2.imwrite('images3D/' + 'hilbert.png', upscale_image(image, upscale=1))
+
+
 def init_colors():
     # Start with a base of 20 colors taken from the nipy_spectral colormap, then add 6 more colors to the base
     cmap = colors.ListedColormap(plt.get_cmap('nipy_spectral')(np.linspace(0, 1, 20)))
@@ -67,10 +227,13 @@ def init_colors():
     colorOptions.pop(11)
     colorOptions.pop(12)
 
+    for colorVal in colorOptions:
+        colorOutput.append(tuple(int(255 * val) for val in colors.ColorConverter.to_rgb(colorVal)))
+
     return colors.ListedColormap(colorOptions)
 
 
-def toggleHeightSlicer(slicer, axis: str, pl, mesh, clip=True):
+def toggleAxisSlicer(slicer, axis: str, pl, mesh, clip=True):
     pl.clear()
     if slicer:
         if clip:
@@ -90,13 +253,13 @@ def enable_slicing(pl, mesh, clip=False):
     sliceDict = {'x': True, 'y': True, 'z': True}
 
     def toggle_x():
-        sliceDict['x'] = toggleHeightSlicer(sliceDict.get('x'), 'x', pl, mesh, clip=clip)
+        sliceDict['x'] = toggleAxisSlicer(sliceDict.get('x'), 'x', pl, mesh, clip=clip)
 
     def toggle_y():
-        sliceDict['y'] = toggleHeightSlicer(sliceDict.get('y'), 'y', pl, mesh, clip=clip)
+        sliceDict['y'] = toggleAxisSlicer(sliceDict.get('y'), 'y', pl, mesh, clip=clip)
 
     def toggle_z():
-        sliceDict['z'] = toggleHeightSlicer(sliceDict.get('z'), 'z', pl, mesh, clip=clip)
+        sliceDict['z'] = toggleAxisSlicer(sliceDict.get('z'), 'z', pl, mesh, clip=clip)
 
     pl.add_key_event('x', toggle_x)
     pl.add_key_event('y', toggle_y)
@@ -107,15 +270,34 @@ def save_slices(xSlices=False, ySlices=False, zSlices=False):
     if xSlices:
         x_slices = [data[x, :, :] for x in reversed(range(Const.LAND_SIZE))]
         for num in range(len(x_slices)):
-            generate_2d_plot(np.rot90(x_slices[num]), "xSlice" + str(num))
+            plot_slice(np.rot90(x_slices[num]), "xSlice" + str(num))
     if ySlices:
         y_slices = [data[:, y, :] for y in range(Const.LAND_SIZE)]
         for num in range(len(y_slices)):
-            generate_2d_plot(np.rot90(y_slices[num]), "ySlice" + str(num))
+            plot_slice(np.rot90(y_slices[num]), "ySlice" + str(num))
     if zSlices:
         z_slices = [data[:, :, z] for z in range(Const.LAND_SIZE)]
         for num in range(len(z_slices)):
-            generate_2d_plot(z_slices[num], "zSlice" + str(num))
+            plot_slice(z_slices[num], "zSlice" + str(num))
+
+
+def generate_terrain():
+    height_map = np.zeros((Const.LAND_SIZE, Const.LAND_SIZE)).astype('float64')
+    height_map += generate_noise(size=Const.LAND_SIZE)
+    height_map = height_map / np.max(np.abs(height_map)) * Const.LAND_SIZE * (40 / 128)
+    return height_map
+
+
+def gen_voxel_colors(grid):
+    freq = (random.randrange(150, 300) / 100, random.randrange(150, 300) / 100, random.randrange(150, 300) / 100)
+    noise3D = pv.perlin_noise(1, freq, (0, 0, 0))
+    grid2 = pv.sample_function(noise3D, [-1, 1.0, -1, 1.0, -1, 1.0],
+                               dim=(Const.LAND_SIZE, Const.LAND_SIZE, Const.LAND_SIZE))
+    grid.cell_data['Colors'] = (np.round(grid2['scalars'] * (Const.NUM_COLORS - 1))) - 0.5
+    grid.cell_data['Colors'] = np.where(grid.cell_data['Colors'] > 0, grid.cell_data['Colors'], -0.5)
+
+    # Label index before removing cells to get back original position
+    grid.cell_data['cell_ind'] = np.arange(grid.GetNumberOfCells())
 
 
 def randomize_colors(grid):
@@ -125,22 +307,17 @@ def randomize_colors(grid):
     grid.cell_data['Colors'] = colorSet
 
 
-def generate(old_noise=False, clip=False, xSlices=False, ySlices=False, zSlices=False):
+def gen_voxels(old_noise):
     # Generate Voxel Space
     x, y, z = np.indices((Const.LAND_SIZE + 1, Const.LAND_SIZE + 1, Const.LAND_SIZE + 1))
     grid = pv.StructuredGrid(z, y, x)
     x, y, z = np.indices((Const.LAND_SIZE, Const.LAND_SIZE, Const.LAND_SIZE))
+    print("Generated Voxel Space")
 
     # 3D Noise Sampling for Colors
-
-    freq = (2, 2, 2)
-    noise3D = pv.perlin_noise(1, freq, (0, 0, 0))
-    grid2 = pv.sample_function(noise3D, [-1, 1.0, -1, 1.0, -1, 1.0],
-                               dim=(Const.LAND_SIZE, Const.LAND_SIZE, Const.LAND_SIZE))
-    grid.cell_data['Colors'] = (np.round(grid2['scalars'] * Const.NUM_COLORS)) - 0.5
-
-    # Label index before removing cells to get back original position
-    grid.cell_data['cell_ind'] = np.arange(grid.GetNumberOfCells())
+    gen_voxel_colors(grid)
+    init_colors()
+    print("Generated Colors")
     # randomize_colors(grid)
 
     global data
@@ -151,9 +328,11 @@ def generate(old_noise=False, clip=False, xSlices=False, ySlices=False, zSlices=
 
         mesh = grid.cast_to_unstructured_grid()
         mesh.remove_cells(np.invert(terrain).flatten(), inplace=True)
+        print("Terrain Made")
 
         data = np.where(terrain.flatten(), grid.cell_data['Colors'], np.nan)
         data = data.reshape((Const.LAND_SIZE, Const.LAND_SIZE, Const.LAND_SIZE))
+
     else:
         grid = grid.threshold(0.2)
         mesh = grid.cast_to_unstructured_grid()
@@ -163,6 +342,14 @@ def generate(old_noise=False, clip=False, xSlices=False, ySlices=False, zSlices=
         for index in range(len(grid.cell_data['cell_ind'])):
             data[(grid.cell_data['cell_ind'])[index]] = grid.cell_data['Colors'][index]
         data = data.reshape((Const.LAND_SIZE, Const.LAND_SIZE, Const.LAND_SIZE))
+        print("Terrain Made")
+
+    return grid, mesh
+
+
+def generate(old_noise=False, clip=False, xSlices=False, ySlices=False, zSlices=False):
+    grid, mesh = gen_voxels(old_noise)
+    print("Generated Voxels")
 
     def printInfo(ok):
         if pl.picked_cell:
@@ -172,13 +359,15 @@ def generate(old_noise=False, clip=False, xSlices=False, ySlices=False, zSlices=
             indexColor = list(grid.cell_data['cell_ind']).index(ind)
             print("Color: ", (grid.cell_data['Colors'])[indexColor])
 
-    pl = pv.Plotter()
-    pl.add_mesh(mesh, show_edges=True, cmap=init_colors(), scalars='Colors', clim=[0, Const.NUM_COLORS])
-    enable_slicing(pl, mesh, clip=clip)
-    pl.enable_element_picking(pickable_window=True, picker=PickerType.CELL, tolerance=0.001, callback=printInfo)
-    save_slices(xSlices, ySlices, zSlices)
-    pl.show(auto_close=False)
+    # pl = pv.Plotter()
+    # pl.add_mesh(mesh, show_edges=True, cmap=init_colors(), scalars='Colors', clim=[0, Const.NUM_COLORS])
+    # enable_slicing(pl, mesh, clip=clip)
+    # pl.enable_element_picking(pickable_window=True, picker=PickerType.CELL, tolerance=0.001, callback=printInfo)
+    # save_slices(xSlices, ySlices, zSlices)
+    sparse_data(data)
+    plot_hilbert(data)
+    # pl.show(auto_close=False)
 
 
 if __name__ == '__main__':
-    generate(old_noise=False, clip=False, zSlices=True)
+    generate(old_noise=True, clip=False, xSlices=True, zSlices=True)
